5563198db7ba0cc427f6427953ea6982808d581e
Long <tin2ktin@gmail.com>
2019-02-28 15:27:56 +0700

    FFC v10

---
+++ scripts/myFunctions.js
@@ -0,0 +1,24 @@
+/* Hoang long
+28/2/2019
+My Function list
+*/
+
+// Thank you alert
+function tyAlert () {
+  alert("Thank you for your interest in our fitness club. We will contact you soon.");
+}
+
+// Burpees video window
+function burpees() {
+  window.open("scripts/burpees.html", "_blank", "width=610, height=360");
+}
+
+// Plank video window
+function plank() {
+  window.open("scripts/plank.html", "_blank", "width=610, height=360");
+}
+
+// Mountain climber video window
+function mtnClimber() {
+  window.open("scripts/mtn.html", "_blank", "width=610, height=360");
+}
